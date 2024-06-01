7387235fb737ebc224c90d94a0b832b19baedf0b
Bindu Hegde <binduhegde1932@gmail.com>
2024-06-01 12:21:13 +0530

    fix bug in fastai model

--- predict_breed.py
+++ predict_breed.py
@@ -1,7 +1,15 @@
 from fastai.learner import load_learner
 
 
+# Function to make predictions
 def predict(image):
-    learn = load_learner('pet_breed_model.pkl')
-    breed, _, probs = learn.predict(image)
-    return breed
+    model = load_learner('pet_breed_model.pkl')
+    _, idx, probabilities = model.predict(image)
+    predicted_class = model.dls.vocab[idx[0]]
+    probability = probabilities[idx[0]].item() * 100
+    return predicted_class, probability
+
+# def predict(image):
+#     learn = load_learner('pet_breed_model.pkl')
+#     breed, _, probs = learn.predict(image)
+#     return breed
